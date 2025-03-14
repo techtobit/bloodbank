c35e83d19a6cf41a78a3cf0c4d821cd4d1111eb0
ashraf <nurthern2016@gmail.com>
2025-03-15 01:58:30 +0600

    setting updated

--- api/settings.py
+++ api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,7 +77,14 @@ WSGI_APPLICATION = 'api.wsgi.app'
 # Note: Django modules for using databases are not support in serverless
 # environments like Vercel. You can use a database over HTTP, hosted elsewhere.
 
-DATABASES = {}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+DATABASES['default'] = dj_database_url.config()
 
 
 # Password validation
--- api/urls.py
+++ api/urls.py
@@ -2,10 +2,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
-# import Users.urls 
+import Users.urls 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('user/', include(Users.urls)),
+    path('user/', include(Users.urls)),
     path('', include('example.urls')),
 ]
